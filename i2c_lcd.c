0f18f2745122e0d03ae9518f85862711790a6815
Maker <Maker>
2024-04-28 08:35:02 +0200

    commented out extra defines

--- i2c_lcd.c
+++ i2c_lcd.c
@@ -19,17 +19,17 @@
 #include <stdio.h>
 
 // Define some device parameters
-#define I2C_ADDR   0x27 // I2C device address
+// #define I2C_ADDR   0x27 // I2C device address
 
 // Define some device constants
-//#define LCD_CHR  1 // Mode - Sending data
-//#define LCD_CMD  0 // Mode - Sending command
+// #define LCD_CHR  1 // Mode - Sending data
+// #define LCD_CMD  0 // Mode - Sending command
 
-#define LINE1  0x80 // 1st line
-#define LINE2  0xC0 // 2nd line
+// #define LINE1  0x80 // 1st line
+// #define LINE2  0xC0 // 2nd line
 
-//#define LCD_BACKLIGHT 0x08  // On
-//#define LCD_BACKLIGHT 0x00  // Off
+// #define LCD_BACKLIGHT 0x08  // On
+// #define LCD_BACKLIGHT 0x00  // Off
 
 // #define ENABLE  0b00000100 // Enable bit
 
